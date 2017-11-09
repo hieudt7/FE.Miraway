4615cf59bc8559829c60453dd76d9e2e72da7d92
hieudt <hieudtx9@gmail.com>
2017-11-09 09:14:47 +0700

    update FE function

---
+++ QR_Solution/scripts/page-script.js
@@ -0,0 +1,57 @@
+﻿function commonFunction() {
+    var videoThumb = '.youtube-thumb';
+    this.generateVideoThumb = function () {
+        $(videoThumb).each(function () {
+            var vidSrc = $(this).attr('data-src'),
+               vidHeight = $(this).attr('data-height'),
+               youtube_video_id = vidSrc.match(/youtube\.com.*(\?v=|\/embed\/)(.{11})/).pop();
+            if (youtube_video_id.length == 11) {
+                var video_thumbnail = $('<img class="yt-thumb" src="//img.youtube.com/vi/' + youtube_video_id + '/0.jpg">');
+                $(this).prepend(video_thumbnail);
+            }
+        });
+    };
+    this.playYoutubeVideo = function () {
+        $(document).on('click', videoThumb, function () {
+            var vidSrc = $(this).attr('data-src'),
+                vidHeight = $(this).attr('data-height'),
+                video_thumbnail = $('<iframe width="100%" height="' + vidHeight + '" src="' + vidSrc + '?autoplay=1" frameborder="0" allowfullscreen></iframe>');
+            $('.yt-thumb').hide();
+            $(this).append(video_thumbnail);
+            $('.ytp-button').hide();
+        })
+    };
+    this.initYoutubeVideo = function () {
+        this.generateVideoThumb();
+        this.playYoutubeVideo();
+    };
+    this.slickMobile = function () {
+        $('.qr-tab').slick({
+            infinite: false,
+            speed: 300,
+            slidesToShow: 8,
+            slidesToScroll: 8,
+            responsive: [
+              {
+                  breakpoint: 767,
+                  settings: {
+                      slidesToShow: 3,
+                      slidesToScroll: 3
+                  }
+              },
+              {
+                  breakpoint: 321,
+                  settings: {
+                      slidesToShow: 2,
+                      slidesToScroll: 2
+                  }
+              }
+            ]
+        });
+        //reset menu
+        //$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+        //    $('.nav-tabs li').removeClass('active');
+        //    $(e.target).parent().addClass('active');
+        //});
+    };
+}
