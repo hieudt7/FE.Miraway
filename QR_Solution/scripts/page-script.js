7c0f35a5aac5ab472c4f3b5a072539dbe8081f0c
flyfarfaraway <phamthanhdat.bk@gmail.com>
2017-12-13 15:54:19 +0700

    pricing

--- QR_Solution/scripts/page-script.js
+++ QR_Solution/scripts/page-script.js
@@ -1,64 +1,56 @@
 ﻿function commonFunction() {
     var videoThumb = '.youtube-thumb';
-    this.generateVideoThumb = function () {
-        $(videoThumb).each(function () {
+    this.generateVideoThumb = function() {
+        $(videoThumb).each(function() {
             var vidSrc = $(this).attr('data-src'),
-               vidHeight = $(this).attr('data-height'),
-               youtube_video_id = vidSrc.match(/youtube\.com.*(\?v=|\/embed\/)(.{11})/).pop();
+                vidHeight = $(this).attr('data-height'),
+                youtube_video_id = vidSrc.match(/youtube\.com.*(\?v=|\/embed\/)(.{11})/).pop();
             if (youtube_video_id.length == 11) {
                 var video_thumbnail = $('<img class="yt-thumb" src="//img.youtube.com/vi/' + youtube_video_id + '/0.jpg">');
                 $(this).prepend(video_thumbnail);
             }
         });
     };
-    this.playYoutubeVideo = function () {
-        $(document).on('click', videoThumb, function () {
+    this.playYoutubeVideo = function() {
+        $(document).on('click', videoThumb, function() {
             var vidSrc = $(this).attr('data-src'),
                 vidHeight = $(this).attr('data-height'),
-<<<<<<< HEAD
-                video_thumbnail = $('<iframe class="frame-youtube" width="auto" height=""' + vidHeight + '" src="' + vidSrc + '?autoplay=1&enablejsapi=1&version=3&playerapiid=ytplayer" frameborder="0" allowfullscreen></iframe>');
-=======
                 video_thumbnail = $('<iframe class="frame-youtube" width="100%" height="' + vidHeight + '" src="' + vidSrc + '?autoplay=1&enablejsapi=1&version=3&playerapiid=ytplayer" frameborder="0" allowfullscreen></iframe>');
->>>>>>> refs/remotes/origin/master
             if (vidSrc !== '' && vidSrc !== undefined) {
                 $(this).find('.yt-thumb').hide();
                 $(this).append(video_thumbnail);
                 $(this).find('.ytp-button').hide();
                 $(this).attr('data-src', '');
             }
-<<<<<<< HEAD
-=======
-            
+
             //pause
             //$('#id_1')[0].contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
->>>>>>> refs/remotes/origin/master
         })
     };
-    this.initYoutubeVideo = function () {
+    this.initYoutubeVideo = function() {
         this.generateVideoThumb();
         this.playYoutubeVideo();
     };
-    this.slickMobile = function () {
+    this.slickMobile = function() {
         $('.qr-tab').slick({
             infinite: false,
             speed: 300,
             slidesToShow: 8,
             slidesToScroll: 8,
-            responsive: [
-              {
-                  breakpoint: 767,
-                  settings: {
-                      slidesToShow: 3,
-                      slidesToScroll: 3
-                  }
-              },
-              {
-                  breakpoint: 321,
-                  settings: {
-                      slidesToShow: 2,
-                      slidesToScroll: 2
-                  }
-              }
+            responsive: [{
+                    breakpoint: 767,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3
+                    }
+                },
+                {
+                    breakpoint: 321,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                }
             ]
         });
         //reset menu
